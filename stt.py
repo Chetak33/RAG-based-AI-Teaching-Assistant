c88a0b8cb03a17d7c00afcb69619b6e12daa782e
chittaranjan <chittaranjanmaske@gmail.com>
2026-02-25 12:07:35 +0530

    created speach to text

--- stt.py
+++ stt.py
@@ -1,3 +1,9 @@
-import whisper
+# import whisper
 
-model = whisper.load_model("large-v2")
+# model = whisper.load_model("large-v2")
+
+result = model.transcribe(audio = "audios/3_This AI can build Mobile Apps with High Accuracy Manus 1.6 Updates Tutorial.mp3",
+                          language="hi",
+                          task="translate",
+                          word_timestamps=False)
+print(result["text"])
