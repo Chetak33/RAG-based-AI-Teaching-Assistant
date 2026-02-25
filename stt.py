791c27cbc3a4abbb15e9d0ccfef196d1d8b1de3f
chittaranjan <chittaranjanmaske@gmail.com>
2026-02-25 17:59:56 +0530

    chunks created

--- stt.py
+++ stt.py
@@ -1,8 +1,8 @@
 # import whisper
 # model = whisper.load_model("large-v2")
 
-# # result = model.transcribe(audio = "audios/3_This AI can build Mobile Apps with High Accuracy Manus 1.6 Updates Tutorial.mp3",
-# #                           language="hi",
-# #                           task="translate",
-# #                           word_timestamps=False)
-# # print(result["text"])
+# result = model.transcribe(audio = "audios/3_This AI can build Mobile Apps with High Accuracy Manus 1.6 Updates Tutorial.mp3",
+#                           language="hi",
+#                           task="translate",
+#                           word_timestamps=False)
+# print(result["text"])
